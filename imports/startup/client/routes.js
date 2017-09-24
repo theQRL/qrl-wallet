d6534c6b87354e83efc0689afc5db1567fdae334
Scott Donald <scottdonald@protonmail.ch>
2017-09-24 12:42:50 +1000

    Adding verify transaction page and cleanup

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,6 +8,8 @@ import '../../ui/pages/not-found/not-found.js'
 import '../../ui/pages/create/create.js'
 import '../../ui/pages/view/view.js'
 import '../../ui/pages/transfer/transfer.js'
+import '../../ui/pages/verify/verify.js'
+
 
 // Set up all routes in the app
 FlowRouter.route('/', {
@@ -23,19 +25,25 @@ FlowRouter.route('/create', {
   },
 })
 FlowRouter.route('/view', {
-  name: 'App.home',
+  name: 'App.view',
   action() {
     BlazeLayout.render('appBody', { main: 'appView' })
   },
 })
 FlowRouter.route('/transfer', {
-  name: 'App.home',
+  name: 'App.transfer',
   action() {
     BlazeLayout.render('appBody', { main: 'appTransfer' })
   },
 })
+FlowRouter.route('/verify', {
+  name: 'App.verify',
+  action() {
+    BlazeLayout.render('appBody', { main: 'appVerify' })
+  },
+})
 FlowRouter.notFound = {
   action() {
-    BlazeLayout.render('appBody', { main: 'appBotFound' })
+    BlazeLayout.render('appBody', { main: 'appNotFound' })
   },
 }
