548f3014a654909f1731ca19e21f5ff2ba3c8b75
JP Lomas <jp.lomas@gmail.com>
2017-09-24 20:10:32 +0100

    Merge create wallet UX

--- imports/ui/pages/create/create.js
+++ imports/ui/pages/create/create.js
@@ -1,1 +1,15 @@
 import './create.html'
+
+Template.appCreate.events({
+  'click #generate': () => {
+    $('#generate').hide()
+    $('#generating').show()
+    // CALL WASM HERE
+  },
+  'click #generating': () => {
+    $('#generating').hide()
+    $('#warning').hide()
+    $('#result').show()
+    // SHOW NEW ADDRESS HERE
+  },
+})
