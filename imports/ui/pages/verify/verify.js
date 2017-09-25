a49ecd2ab8f3cca1111a4b62e759b76e44c23434
Scott Donald <scottdonald@protonmail.ch>
2017-09-25 21:25:23 +1000

    Adding view wallet UI

--- imports/ui/pages/verify/verify.js
+++ imports/ui/pages/verify/verify.js
@@ -4,8 +4,10 @@ Template.appVerify.events({
   'click .button': function() {
     const verifyTxnPath = '/verify-txid/:txId'
     const transactionId = document.getElementById('transactionId').value
-    const params = { txId: transactionId }
-    const path = FlowRouter.path(verifyTxnPath, params)
-    FlowRouter.go(path)
+    if (transactionId !== '') {
+      const params = { txId: transactionId }
+      const path = FlowRouter.path(verifyTxnPath, params)
+      FlowRouter.go(path)
+    }
   },
 })
