8a7df592a39b4267de0fdfaa0a10b30ffba8e64c
Scott Donald <scottdonald@protonmail.ch>
2017-09-27 08:59:59 +1000

    Removing Meteor server functionality, and general cleanup

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -1,2 +1,1 @@
 // Import modules used by both client and server through a single index entry point
-import './nodes.js'
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,2 +1,4 @@
 // Import client startup through a single index entry point
 import './routes.js'
+import './nodes.js'
+import './web-libjsqrl.js'
