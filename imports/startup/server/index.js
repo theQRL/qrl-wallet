ba74130db700dce5b949d66b86515f61ad34e33e
Scott Donald <scottdonald@protonmail.ch>
2018-03-16 13:00:48 +1100

    Fixes for multiple outputs

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -590,6 +590,203 @@ Meteor.methods({
     const response = Meteor.wrapAsync(getTxnHash)(request)
     return response
   },
+
+
+  txhash(request) {
+    this.unblock()
+    check(request, Object)
+    if (qrlClient[request.grpc] == null) {
+      console.log(`No active grpc connection available - connecting to: ${request.grpc}`)
+      Meteor.wrapAsync(connectToNode)(request)
+    }
+
+    const response = Meteor.wrapAsync(getTxnHash)(request)
+
+    // refactor response data
+    const output = response
+    // console.log(res)
+    if (response.transaction.header) {
+      output.transaction.header.hash_header = Buffer.from(output.transaction.header.hash_header).toString('hex')
+      output.transaction.header.hash_header_prev = Buffer.from(output.transaction.header.hash_header_prev).toString('hex')
+      output.transaction.header.merkle_root = Buffer.from(output.transaction.header.merkle_root).toString('hex')
+      output.transaction.header.PK = Buffer.from(output.transaction.header.PK).toString('hex')
+
+      output.transaction.tx.transaction_hash = Buffer.from(output.transaction.tx.transaction_hash).toString('hex')
+      output.transaction.tx.amount = ''
+
+      if (output.transaction.tx.transactionType === 'coinbase') {
+        output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+        output.transaction.tx.addr_to = 'Q' + Buffer.from(output.transaction.tx.coinbase.addr_to).toString('hex')
+        output.transaction.tx.coinbase.addr_to = 'Q' + Buffer.from(output.transaction.tx.coinbase.addr_to).toString('hex')
+        output.transaction.tx.amount = numberToString(output.transaction.tx.coinbase.amount / SHOR_PER_QUANTA)
+
+        output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+        output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+        output.transaction.tx.coinbase.headerhash = Buffer.from(output.transaction.tx.coinbase.headerhash).toString('hex')
+
+        output.transaction.explorer = {
+          from: '',
+          to: output.transaction.tx.addr_to,
+          type: 'COINBASE',
+        }
+      }
+    } else {
+      output.transaction.tx.transaction_hash = Buffer.from(output.transaction.tx.transaction_hash).toString('hex')
+    }
+
+    if (output.transaction.tx.transactionType === 'token') {
+      const balances = []
+      output.transaction.tx.token.initial_balances.forEach((value) => {
+        const edit = value
+        edit.address = 'Q' + Buffer.from(edit.address).toString('hex'),
+        edit.amount = numberToString(edit.amount / Math.pow(10, output.transaction.tx.token.decimals))
+        balances.push(edit)
+      })
+
+      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+
+      output.transaction.tx.token.symbol = Buffer.from(output.transaction.tx.token.symbol).toString('hex')
+      output.transaction.tx.token.name = Buffer.from(output.transaction.tx.token.name).toString('hex')
+      output.transaction.tx.token.owner = 'Q' + Buffer.from(output.transaction.tx.token.owner).toString('hex')
+
+      output.transaction.tx.fee = numberToString(output.transaction.tx.fee / SHOR_PER_QUANTA)
+      output.transaction.explorer = {
+        from: output.transaction.tx.addr_from,
+        to: output.transaction.tx.addr_from,
+        signature: output.transaction.tx.signature,
+        publicKey: output.transaction.tx.public_key,
+        symbol: output.transaction.tx.token.symbol,
+        name: output.transaction.tx.token.name,
+        decimals: output.transaction.tx.token.decimals,
+        owner: output.transaction.tx.token.owner,
+        initialBalances: balances,
+        type: 'CREATE TOKEN',
+      }
+    }
+    
+    if (output.transaction.tx.transactionType === 'transfer') {
+      // Calculate total transferred, and generate a clean structure to display outputs from
+      let thisTotalTransferred = 0
+      let thisOutputs = []
+      _.each(output.transaction.tx.transfer.addrs_to, (thisAddress, index) => {
+        const thisOutput = {
+          address: 'Q' + Buffer.from(thisAddress).toString('hex'),
+          amount: numberToString(output.transaction.tx.transfer.amounts[index] / SHOR_PER_QUANTA)
+        }
+        thisOutputs.push(thisOutput)
+
+        // Now update total transferred with the corresponding amount from this output
+        thisTotalTransferred = thisTotalTransferred + output.transaction.tx.transfer.amounts[index]
+      })
+
+      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+      output.transaction.tx.transfer.outputs = thisOutputs
+      output.transaction.tx.amount = numberToString(thisTotalTransferred / SHOR_PER_QUANTA)
+      output.transaction.tx.fee = numberToString(output.transaction.tx.fee / SHOR_PER_QUANTA)
+      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+     
+      output.transaction.explorer = {
+        from: output.transaction.tx.addr_from,
+        outputs: thisOutputs,
+        totalTransferred: numberToString(thisTotalTransferred / SHOR_PER_QUANTA),
+        type: 'TRANSFER',
+      }
+    }
+
+    if (output.transaction.tx.transactionType === 'transfer_token') {
+
+      // Request Token Decimals / Symbol
+      const symbolRequest = {
+        query: Buffer.from(output.transaction.tx.transfer_token.token_txhash, 'hex'),
+        grpc: request.grpc
+      }
+      const thisSymbolResponse = Meteor.wrapAsync(getTxnHash)(symbolRequest)
+      const thisSymbol = Buffer.from(thisSymbolResponse.transaction.tx.token.symbol).toString()
+      const thisDecimals = thisSymbolResponse.transaction.tx.token.decimals
+
+      // Calculate total transferred, and generate a clean structure to display outputs from
+      let thisTotalTransferred = 0
+      let thisOutputs = []
+      _.each(output.transaction.tx.transfer_token.addrs_to, (thisAddress, index) => {
+        const thisOutput = {
+          address: 'Q' + Buffer.from(thisAddress).toString('hex'),
+          amount: numberToString(output.transaction.tx.transfer_token.amounts[index] / Math.pow(10, thisDecimals))
+        }
+        thisOutputs.push(thisOutput)
+
+        // Now update total transferred with the corresponding amount from this output
+        thisTotalTransferred = thisTotalTransferred + output.transaction.tx.transfer_token.amounts[index]
+      })
+
+      output.transaction.tx.fee = numberToString(output.transaction.tx.fee / SHOR_PER_QUANTA)
+      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+      output.transaction.tx.transfer_token.token_txhash = Buffer.from(output.transaction.tx.transfer_token.token_txhash).toString('hex')
+      output.transaction.tx.transfer_token.outputs = thisOutputs
+      output.transaction.tx.totalTransferred = numberToString(thisTotalTransferred / Math.pow(10, thisDecimals))
+
+      output.transaction.explorer = {
+        from: output.transaction.tx.addr_from,
+        outputs: thisOutputs,
+        signature: output.transaction.tx.signature,
+        publicKey: output.transaction.tx.public_key,
+        token_txhash: output.transaction.tx.transfer_token.token_txhash,
+        totalTransferred: numberToString(thisTotalTransferred / Math.pow(10, thisDecimals)),
+        type: 'TRANSFER TOKEN',
+      }
+    }
+
+    if (output.transaction.tx.transactionType === 'slave') {
+      output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
+
+      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+
+      output.transaction.tx.slave.slave_pks.forEach((value, index) => {
+        output.transaction.tx.slave.slave_pks[index] = 
+          Buffer.from(value).toString('hex')
+      })
+
+      output.transaction.explorer = {
+        from: output.transaction.tx.addr_from,
+        to: '',
+        signature: output.transaction.tx.signature,
+        publicKey: output.transaction.tx.public_key,
+        amount: output.transaction.tx.amount,
+        type: 'SLAVE',
+      }
+    }
+
+    if (output.transaction.tx.transactionType === 'latticePK') {
+      output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
+
+      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
+      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
+      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
+
+      output.transaction.tx.latticePK.kyber_pk = Buffer.from(output.transaction.tx.latticePK.kyber_pk).toString('hex')
+      output.transaction.tx.latticePK.dilithium_pk = Buffer.from(output.transaction.tx.latticePK.dilithium_pk).toString('hex')
+
+      output.transaction.explorer = {
+        from: output.transaction.tx.addr_from,
+        to: '',
+        signature: output.transaction.tx.signature,
+        publicKey: output.transaction.tx.public_key,
+        amount: output.transaction.tx.amount,
+        type: 'LATTICE PK',
+      }
+    }
+    return output
+  },
+
+
+
+
   transferCoins(request) {
     this.unblock()
     check(request, Object)
@@ -679,6 +876,7 @@ Meteor.methods({
             from: thisTxnHashResponse.transaction.tx.addr_from,
             symbol: Buffer.from(thisTxnHashResponse.transaction.tx.token.symbol).toString(),
             name: Buffer.from(thisTxnHashResponse.transaction.tx.token.name).toString(),
+            decimals: thisTxnHashResponse.transaction.tx.token.decimals,
             ots_key: parseInt(thisTxnHashResponse.transaction.tx.signature.substring(0, 8), 16),
             fee: thisTxnHashResponse.transaction.tx.fee / SHOR_PER_QUANTA,
             block: thisTxnHashResponse.transaction.header.block_number,
@@ -731,6 +929,34 @@ Meteor.methods({
             amount: thisTxnHashResponse.transaction.tx.coinbase.amount / SHOR_PER_QUANTA,
             from: thisTxnHashResponse.transaction.tx.addr_from,
             to: thisTxnHashResponse.transaction.tx.coinbase.addr_to,
+            ots_key: parseInt(thisTxnHashResponse.transaction.tx.signature.substring(0, 8), 16),
+            fee: thisTxnHashResponse.transaction.tx.fee / SHOR_PER_QUANTA,
+            block: thisTxnHashResponse.transaction.header.block_number,
+            timestamp: thisTxnHashResponse.transaction.header.timestamp_seconds,
+          }
+
+          result.push(thisTxn)
+        } else if (thisTxnHashResponse.transaction.tx.transactionType == "slave") {
+          thisTxn = {
+            type: thisTxnHashResponse.transaction.tx.transactionType,
+            txhash: arr.txhash,
+            amount: 0,
+            from: thisTxnHashResponse.transaction.tx.addr_from,
+            to: '',
+            ots_key: parseInt(thisTxnHashResponse.transaction.tx.signature.substring(0, 8), 16),
+            fee: thisTxnHashResponse.transaction.tx.fee / SHOR_PER_QUANTA,
+            block: thisTxnHashResponse.transaction.header.block_number,
+            timestamp: thisTxnHashResponse.transaction.header.timestamp_seconds,
+          }
+
+          result.push(thisTxn)
+        } else if (thisTxnHashResponse.transaction.tx.transactionType == "latticePK") {
+          thisTxn = {
+            type: thisTxnHashResponse.transaction.tx.transactionType,
+            txhash: arr.txhash,
+            amount: 0,
+            from: thisTxnHashResponse.transaction.tx.addr_from,
+            to: '',
             ots_key: parseInt(thisTxnHashResponse.transaction.tx.signature.substring(0, 8), 16),
             fee: thisTxnHashResponse.transaction.tx.fee / SHOR_PER_QUANTA,
             block: thisTxnHashResponse.transaction.header.block_number,
--- imports/ui/pages/verify/tx.js
+++ imports/ui/pages/verify/tx.js
@@ -10,128 +10,6 @@ import './tx.html'
 /* ^^^ remove once testing complete
  */
 
-const ab2str = buf => String.fromCharCode.apply(null, new Uint16Array(buf))
-
-const txResultsRefactor = (res) => {
-  // rewrite all arrays as strings (Q-addresses) or hex (hashes)
-  const output = res
-  // console.log(res)
-  if (res.transaction.header) {
-    output.transaction.header.hash_header = Buffer.from(output.transaction.header.hash_header).toString('hex')
-    output.transaction.header.hash_header_prev = Buffer.from(output.transaction.header.hash_header_prev).toString('hex')
-    output.transaction.header.merkle_root = Buffer.from(output.transaction.header.merkle_root).toString('hex')
-    output.transaction.header.PK = Buffer.from(output.transaction.header.PK).toString('hex')
-
-    output.transaction.tx.transaction_hash = Buffer.from(output.transaction.tx.transaction_hash).toString('hex')
-    // output.transaction.tx.addr_to = ''
-    output.transaction.tx.amount = ''
-
-    if (output.transaction.tx.transactionType === 'coinbase') {
-      output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
-      output.transaction.tx.addr_to = 'Q' + Buffer.from(output.transaction.tx.coinbase.addr_to).toString('hex')
-      output.transaction.tx.coinbase.addr_to = 'Q' + Buffer.from(output.transaction.tx.coinbase.addr_to).toString('hex')
-      output.transaction.tx.amount = output.transaction.tx.coinbase.amount / SHOR_PER_QUANTA
-
-      output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
-      output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
-      output.transaction.tx.coinbase.headerhash = Buffer.from(output.transaction.tx.coinbase.headerhash).toString('hex')
-
-      output.transaction.explorer = {
-        from: '',
-        to: output.transaction.tx.addr_to,
-        type: 'COINBASE',
-      }
-    }
-  } else {
-    output.transaction.tx.transaction_hash = Buffer.from(output.transaction.tx.transaction_hash).toString('hex')
-  }
-
-  if (output.transaction.tx.transactionType === 'transfer') {
-    output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
-    output.transaction.tx.addr_to = 'Q' + Buffer.from(output.transaction.tx.transfer.addr_to).toString('hex')
-    output.transaction.tx.transfer.addr_to = 'Q' + Buffer.from(output.transaction.tx.transfer.addr_to).toString('hex')
-    output.transaction.tx.amount = output.transaction.tx.transfer.amount / SHOR_PER_QUANTA
-    output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
-    output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
-    output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
-    output.transaction.explorer = {
-      from: output.transaction.tx.addr_from,
-      to: output.transaction.tx.addr_to,
-      type: 'TRANSFER',
-    }
-  }
-
-  if (output.transaction.tx.transactionType === 'token') {
-    const balances = []
-    output.transaction.tx.token.initial_balances.forEach((value) => {
-      const edit = value
-      edit.address = 'Q' + Buffer.from(edit.address).toString('hex'),
-      edit.amount = edit.amount / Math.pow(10, output.transaction.tx.token.decimals)
-      balances.push(edit)
-    })
-
-    output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
-    output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
-    output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
-
-    output.transaction.tx.token.symbol = ab2str(output.transaction.tx.token.symbol)
-    output.transaction.tx.token.name = ab2str(output.transaction.tx.token.name)
-    output.transaction.tx.token.owner = 'Q' + Buffer.from(output.transaction.tx.token.owner).toString('hex')
-
-    output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
-    output.transaction.explorer = {
-      from: output.transaction.tx.addr_from,
-      to: output.transaction.tx.addr_from,
-      signature: output.transaction.tx.signature,
-      publicKey: output.transaction.tx.public_key,
-      symbol: output.transaction.tx.token.symbol,
-      name: output.transaction.tx.token.name,
-      owner: output.transaction.tx.token.owner,
-      initialBalances: balances,
-      type: 'CREATE TOKEN',
-    }
-  }
-  
-  if (output.transaction.tx.transactionType === 'transfer_token') {
-    output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
-
-    output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
-    output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
-    output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
-    output.transaction.tx.transfer_token.addr_to = 'Q' + Buffer.from(output.transaction.tx.transfer_token.addr_to).toString('hex')
-    output.transaction.tx.transfer_token.token_txhash = Buffer.from(output.transaction.tx.transfer_token.token_txhash).toString('hex')
-    
-    output.transaction.explorer = {
-      from: output.transaction.tx.addr_from,
-      to: output.transaction.tx.transfer_token.addr_to,
-      signature: output.transaction.tx.signature,
-      publicKey: output.transaction.tx.public_key,
-      token_txhash: output.transaction.tx.transfer_token.token_txhash,
-      amount: output.transaction.tx.transfer_token.amount / SHOR_PER_QUANTA,
-      type: 'TRANSFER TOKEN',
-    }
-  }
-
-  if (output.transaction.tx.transactionType === 'slave') {
-    output.transaction.tx.fee = output.transaction.tx.fee / SHOR_PER_QUANTA
-
-    output.transaction.tx.public_key = Buffer.from(output.transaction.tx.public_key).toString('hex')
-    output.transaction.tx.signature = Buffer.from(output.transaction.tx.signature).toString('hex')
-    output.transaction.tx.addr_from = 'Q' + Buffer.from(output.transaction.tx.addr_from).toString('hex')
-
-    output.transaction.explorer = {
-      from: output.transaction.tx.addr_from,
-      to: '',
-      signature: output.transaction.tx.signature,
-      publicKey: output.transaction.tx.public_key,
-      amount: output.transaction.tx.amount,
-      type: 'SLAVE',
-    }
-  }
-  return output
-}
-
-
 Template.appVerifyTxid.onRendered(() => {
   this.$('.value').popup()
 
@@ -147,11 +25,11 @@ Template.appVerifyTxid.onRendered(() => {
   }
 
   if (thisTxId) {
-    Meteor.call('getTxnHash', request, (err, res) => {
+    Meteor.call('txhash', request, (err, res) => {
       if (err) {
         LocalStore.set('txhash', { error: err, id: thisTxId })
       } else {
-        LocalStore.set('txhash', txResultsRefactor(res))
+        LocalStore.set('txhash', res)
       }
     })
 
@@ -206,8 +84,11 @@ Template.appVerifyTxid.helpers({
     if (this.tx.coinbase) {
       return (this.tx.coinbase.amount / SHOR_PER_QUANTA).toFixed(9)
     }
-    if (this.tx.transfer) {
-      return (this.tx.transfer.amount / SHOR_PER_QUANTA).toFixed(9)
+    if (this.tx.transactionType === 'transfer') {
+      return numberToString(this.tx.transfer.totalTransferred) + " Quanta"
+    }
+    if (this.tx.transactionType === 'transfer_token') {
+      return numberToString(this.tx.transfer_token.totalTransferred) + " " + this.tx.transfer_token.tokenSymbol
     }
     return ''
   },
@@ -237,6 +118,12 @@ Template.appVerifyTxid.helpers({
   },
   isToken() {
     if (this.explorer.type === 'CREATE TOKEN') {
+      return true
+    }
+    return false
+  },
+  isTransfer() {
+    if (this.explorer.type === 'TRANSFER') {
       return true
     }
     return false
