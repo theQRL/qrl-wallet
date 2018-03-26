e656fdf7f99c11f028a4c170fb1fdcd7f684881b
Scott Donald <scottdonald@protonmail.ch>
2018-03-26 17:18:48 +1100

    Fixing exponent display bug

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -9,25 +9,6 @@ WALLET_VERSION="0.3.0"
 
 // Function to cleanly represent large decimal numbers without exponentional formatting.
 numberToString = (num) => {
-  let numStr = String(num)
-
-  if (Math.abs(num) < 1.0) {
-    let e = parseInt(num.toString().split('e-')[1])
-    if (e) {
-      let negative = num < 0;
-      if (negative) num *= -1
-      num *= Math.pow(10, e - 1)
-      numStr = '0.' + (new Array(e)).join('0') + num.toString().substring(2)
-      if (negative) numStr = "-" + numStr
-    }
-  } else {
-    let e = parseInt(num.toString().split('+')[1])
-    if (e > 20) {
-      e -= 20;
-      num /= Math.pow(10, e)
-      numStr = num.toString() + (new Array(e + 1)).join('0')
-    }
-  }
-
-  return numStr;
+  const math = require('mathjs')
+  return math.format(num,{exponential:{lower:0,upper:Infinity}})
 }
